e28912ff84792625de5cdd55645243d6960910ed
ntl-arslan <arslanali.irshad@nayatel.com>
2025-11-26 15:46:19 +0500

    db connections

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [],
+imports: [
+  ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: '.env',
+  }),
+
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    autoLoadEntities: true,
+  }),
+]
+
+,
+
   controllers: [AppController],
   providers: [AppService],
 })
