8518f00e26e7fcd2c8f24dcc057bc98114f77728
ntl-arslan <arslanali.irshad@nayatel.com>
2025-11-27 15:02:52 +0500

    cors allowed

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  app.enableCors({
+    origin: '*', // allow all origins; you can replace '*' with specific domains for security
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // if you want to allow cookies/auth headers
+  });
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
