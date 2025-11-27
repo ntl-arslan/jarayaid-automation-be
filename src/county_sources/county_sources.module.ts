508fd850603254541c19c63b28246ee72ac98255
ntl-arslan <arslanali.irshad@nayatel.com>
2025-11-27 17:11:53 +0500

    country with source working updated

--- src/county_sources/county_sources.module.ts
+++ src/county_sources/county_sources.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CountySourcesService } from './county_sources.service';
 import { CountySourcesController } from './county_sources.controller';
-import { CountriesInfo } from './entities/county_source.entity';
+import { CountriesInfo } from './entities/county_info.entity';
+import { CountrySources } from './entities/country_source.entity';
 
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CountriesInfo])],
+  imports: [TypeOrmModule.forFeature([CountriesInfo,CountrySources])],
   controllers: [CountySourcesController],
   providers: [CountySourcesService],
 })
